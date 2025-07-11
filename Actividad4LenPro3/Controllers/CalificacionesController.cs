0989e146366cf60ee363a7dba4896a27f526ef5d
Yoldis Rodriguez <yoldisrodriguezherrera@gmail.com>
2025-07-11 06:38:17 -0400

    CRUD - Calificaciones

---
+++ Actividad4LenPro3/Controllers/CalificacionesController.cs
@@ -0,0 +1,156 @@
+﻿using Actividad4LenPro3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Actividad4LenPro3.Controllers
+{
+    public class CalificacionesController : Controller
+    {
+        private readonly Actividad4LenPro3Context _context;
+
+        public CalificacionesController(Actividad4LenPro3Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Estudiantes = _context.Estudiantes
+                .Select(e => new SelectListItem
+                {
+                    Value = e.Matricula,
+                    Text = e.Matricula + " - " + e.NombreCompleto
+                }).ToList();
+
+            ViewBag.Materias = _context.Materias
+                .Select(m => new SelectListItem
+                {
+                    Value = m.Codigo,
+                    Text = m.Codigo + " - " + m.Nombre
+                }).ToList();
+
+            return View("Crear");
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(Calificacione model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                ViewBag.Estudiantes = _context.Estudiantes
+                    .Select(e => new SelectListItem
+                    {
+                        Value = e.Matricula,
+                        Text = e.Matricula + " - " + e.NombreCompleto
+                    }).ToList();
+
+                ViewBag.Materias = _context.Materias
+                    .Select(m => new SelectListItem
+                    {
+                        Value = m.Codigo,
+                        Text = m.Codigo + " - " + m.Nombre
+                    }).ToList();
+
+                return View("Crear", model);
+            }
+
+            _context.Calificaciones.Add(model);
+            _context.SaveChanges();
+
+            TempData["Mensaje"] = "¡Calificación registrada correctamente!";
+            return RedirectToAction("Lista");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var calificacion = _context.Calificaciones.FirstOrDefault(e => e.Id == id);
+
+            if (calificacion == null)
+            {
+                ViewBag.Mensaje = "La calificacion no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            ViewBag.Estudiantes = _context.Estudiantes
+                   .Select(e => new SelectListItem
+                   {
+                       Value = e.Matricula,
+                       Text = e.Matricula + " - " + e.NombreCompleto
+                   }).ToList();
+
+            ViewBag.Materias = _context.Materias
+                .Select(m => new SelectListItem
+                {
+                    Value = m.Codigo,
+                    Text = m.Codigo + " - " + m.Nombre
+                }).ToList();
+            return View("Editar", calificacion);
+        }
+
+
+        [HttpPost]
+        public IActionResult Editar(Calificacione calificacion)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Editar", calificacion);
+            }
+
+            var calificacionActual = _context.Calificaciones.FirstOrDefault(e => e.Id == calificacion.Id);
+
+            if (calificacionActual == null)
+            {
+                ViewBag.Mensaje = "La materia no existe.";
+                return RedirectToAction("Lista");
+            }
+
+
+            calificacionActual.MatriculaEstudiante = calificacion.MatriculaEstudiante;
+            calificacionActual.CodigoMateria = calificacion.CodigoMateria;
+            calificacionActual.Nota = calificacion.Nota;
+            calificacionActual.Periodo = calificacion.Periodo;
+
+            var rowCounts = _context.SaveChanges();
+
+            if (rowCounts > 0)
+            {
+                ViewBag.Mensaje = "¡Calificaion modificada correctamente!";
+            }
+            else
+            {
+                ViewBag.Mensaje = "No se realizaron cambios en la calificacion.";
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            var calificaciones = _context.Calificaciones.ToList();
+            return View(calificaciones);
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            var calificacion = _context.Calificaciones.FirstOrDefault(c => c.Id == id);
+            if (calificacion == null)
+            {
+                TempData["Mensaje"] = "La calificación no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            _context.Calificaciones.Remove(calificacion);
+            _context.SaveChanges();
+
+            TempData["Mensaje"] = "Calificación eliminada correctamente.";
+            return RedirectToAction("Lista");
+        }
+    }
+}
--- Actividad4LenPro3/Models/Calificacione.cs
+++ Actividad4LenPro3/Models/Calificacione.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,16 +9,18 @@ public partial class Calificacione
 {
     [Key]
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage ="La matricula del estudiante es requerida")]
     public string MatriculaEstudiante { get; set; } = null!;
-    [Required]
+    [Required(ErrorMessage ="El codigo de la materia es requerido")]
     public string CodigoMateria { get; set; } = null!;
-    [Required, Range(0, 100)]
+    [Required(ErrorMessage ="La nota es requerida")]
+    [Range(0, 100, ErrorMessage ="La calificacion debe ser entre 0 y 100")]
     public double Nota { get; set; }
-    [Required]
+    [Required(ErrorMessage ="El periodo es requerido")]
     public string Periodo { get; set; } = null!;
 
+    [ValidateNever]
     public virtual Materia CodigoMateriaNavigation { get; set; } = null!;
-
+    [ValidateNever]
     public virtual Estudiante MatriculaEstudianteNavigation { get; set; } = null!;
 }
