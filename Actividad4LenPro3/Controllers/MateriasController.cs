958574fc85a375e5d3b8cb271283d3e0cdcd41de
Yoldis Rodriguez <yoldisrodriguezherrera@gmail.com>
2025-07-11 05:05:39 -0400

    CRUD - Materias

---
+++ Actividad4LenPro3/Controllers/MateriasController.cs
@@ -0,0 +1,133 @@
+﻿using Actividad4LenPro3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad4LenPro3.Controllers
+{
+    public class MateriasController : Controller
+    {
+        private readonly Actividad4LenPro3Context _context;
+
+        public MateriasController(Actividad4LenPro3Context context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View("Crear");
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(Materia model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Crear", model);
+            }
+
+            _context.Materias.Add(model);
+            var rowCounts = _context.SaveChanges();
+
+            if (rowCounts > 0)
+            {
+                ViewBag.Mensaje = "Materia registrada correctamente!";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Algo salio mal al registrar la materia";
+
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            var materias = _context.Materias.ToList();
+            return View(materias);
+        }
+
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var materia = _context.Materias.FirstOrDefault(e => e.Id == id);
+
+            if (materia == null)
+            {
+                ViewBag.Mensaje = "La materia no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            return View("Editar", materia);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Editar(Materia materia)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Editar", materia);
+            }
+
+            var materiaActual = _context.Materias.FirstOrDefault(e => e.Id == materia.Id);
+
+            if (materiaActual == null)
+            {
+                ViewBag.Mensaje = "La materia no existe.";
+                return RedirectToAction("Lista");
+            }
+
+
+            materiaActual.Nombre = materia.Nombre;
+            materiaActual.Creditos = materia.Creditos;
+            materiaActual.Carrera = materia.Carrera;
+
+            var rowCounts = _context.SaveChanges();
+
+            if (rowCounts > 0)
+            {
+                ViewBag.Mensaje = "¡Materia modificada correctamente!";
+            }
+            else
+            {
+                ViewBag.Mensaje = "No se realizaron cambios en la materia.";
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            var materia = _context.Materias.FirstOrDefault(e => e.Id == id);
+            if (materia == null)
+            {
+                ViewBag.Mensaje = "La materia no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            _context.Materias.Remove(materia);
+            var rowCounts = _context.SaveChanges();
+            if (rowCounts > 0)
+            {
+                ViewBag.Mensaje = "Materia eliminado correctamente!";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Algo salio mal al eliminar el materia";
+
+            }
+            return RedirectToAction("Lista");
+        }
+    }
+}
--- Actividad4LenPro3/Models/Materia.cs
+++ Actividad4LenPro3/Models/Materia.cs
@@ -8,11 +8,14 @@ public partial class Materia
 {
     [Key]
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage ="El nombre es requerido")]
+    public string Nombre { get; set; } = null!;
+    [Required(ErrorMessage = "El codigo es requerido")]
     public string Codigo { get; set; } = null!;
-    [Required, Range(0,10)]
+    [Required(ErrorMessage ="Los creditos son requeridos")]
+    [Range(0, 10, ErrorMessage ="Los creditos deben estar entre 0 y 10")]
     public int Creditos { get; set; }
-    [Required]
+    [Required(ErrorMessage ="La carrera es requerida")]
     public string Carrera { get; set; } = null!;
 
     public virtual ICollection<Calificacione> Calificaciones { get; set; } = new List<Calificacione>();
